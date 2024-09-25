bd364a822281cc8fe0e154411b0fda5f48087a77
Misha373 <misasapka1@gmail.com>
2024-09-25 21:29:23 +0300

    fix bag

--- HW10.1/Program.cs
+++ HW10.1/Program.cs
@@ -8,7 +8,7 @@ for (int i = 0; i < fullnamearrage.Length-1; i++)
     name1 = fullnamearrage[i];
     name2 = fullnamearrage[i + 1];
 }
-if(name1.Substring(0, 1) == name2.Substring(0, 1))
+if (name1[0] == name2[0])
 {
     Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
 }
@@ -16,4 +16,3 @@ else
 {
     Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
 }
-    
